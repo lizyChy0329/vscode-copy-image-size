119f16eebb9a17c811d6c62b5282911989c42743
lizyChy0329 <lizy0329@163.com>
2024-09-30 16:12:01 +0800

    add views target

--- views/vite.config.ts
+++ views/vite.config.ts
@@ -4,5 +4,15 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'target',
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
   plugins: [vue(), UnoCSS()],
 })
