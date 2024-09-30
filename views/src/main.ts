aa7850831d12b8c2a377bf59da60b9646668cc98
lizyChy0329 <lizy0329@163.com>
2024-09-30 10:25:35 +0800

    update dependency

--- views/src/main.ts
+++ views/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
+import '@unocss/reset/tailwind-compat.css'
+import 'virtual:uno.css'
 
 createApp(App).mount('#app')
